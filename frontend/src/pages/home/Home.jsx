4f759e84861495dfd8d8bb9a5a5fb7673114a4be
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-07-13 18:52:35 +0530

    Height changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import SideBar from '../../components/sidebar/Sidebar'
 import MessageContainer from '../../components/message-container/MessageContainer'
 
 function Home() {
-  return <div className='flex flex-col md:flex-row items-stretch md:items-center justify-center bg-gray-600 h-screen md:h-auto rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-40 w-full md:w-auto'>
+  return <div className='flex flex-col md:flex-row items-stretch md:items-center justify-center bg-gray-600 h-screen md:h-[650px] p-4 md:p-0 rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-40 w-full md:w-auto'>
     <SideBar />
     <MessageContainer />
   </div>
