e7a9b3e686b14ff937119f3521778ddd3cd16727
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-07-16 23:13:46 +0530

    cookie expiry

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ function generateTokenAndSetCookie(userId, res){
     });
     
     res.cookie('jwt', token, {
-        maxAge: 150 * 24 * 60 * 60 * 1000, // milliseconds
+        maxAge: 15 * 24 * 60 * 60 * 1000, // milliseconds
         httpOnly: true,               // prevent XSS attacks
         sameSite: "strict",           // prevent CSRF attacks
         secure: process.env.NODE_ENV !== "development"
